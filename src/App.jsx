5b510b296484cf97509d7f88725bee4593207c6f
Rita Bradley <rita@ritabradley.dev>
2023-02-08 20:25:45 -0800

    add styles with DaisyUI and Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,22 @@ import ProfileCard from "./components/ProfileCard";
 function App() {
 
 	return (
-		<div>
-			<h1>Personal Digital Assistants</h1>
-			{pdas.map((pda) => {
-				return (
-					<ProfileCard
-						imgUrl={pda.imgUrl}
-						name={pda.name}
-						handle={pda.handle}
-						bio={pda.bio}
-					/>
-				);
-			})}
+		<div className="flex flex-col justify-center items-center min-h-screen">
+			<h1
+			className=' text-3xl md:text-5xl text-secondary font-bold my-10'>Personal Digital Assistants</h1>
+			<div className="flex flex-col justify-center md: mx-10 align-center md:flex-row space-y-10 md:space-x-10 md:space-y-0">
+				{pdas.map((pda) => {
+					return (
+						<ProfileCard
+							key={pda.name}
+							imgUrl={pda.imgUrl}
+							name={pda.name}
+							handle={pda.handle}
+							bio={pda.bio}
+						/>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const ProfileCard = ({imgUrl, name, handle, bio}) => {
 	return (
-		<div>
-			<img src={imgUrl} alt={name}/>
-			<div>
-				<h3>{name}</h3>
-				<h4>{handle}</h4>
-				<p>{bio}</p>
+		<div className="card card-compact w-72 md:w-1/3 bg-neutral shadow-xl hover:scale-105 transition-transform duration-500">
+			<figure className="bg-white">
+				<img className="w-48 m-auto py-10"  src={imgUrl} alt={name}/>
+			</figure>
+			<div className="card-body prose">
+				<h3 className="card-title text-2xl md:text-3xl text-primary">{name}</h3>
+				<h4 className="text-accent text-lg md:text-xl">{handle}</h4>
+				<p className="w-fit text-base text-base-content">{bio}</p>
 			</div>
 		</div>
 	)
