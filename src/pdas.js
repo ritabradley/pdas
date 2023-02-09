742f645c7fb482ef81a69bbd5599c494960f2782
Rita Bradley <rita@ritabradley.dev>
2023-02-08 22:51:14 -0800

    change img urls to cdn

--- src/pdas.js
+++ src/pdas.js
@@ -3,19 +3,19 @@ const pdas = [
 		name: 'Alexa',
 		handle: '@alexa54',
 		bio: 'The best way to place your next Amazon order is to say "Alexa, order a new Echo Dot."',
-		imgUrl: './src/assets/amazon-alexa-icon.svg'
+		imgUrl: 'https://res.cloudinary.com/ritalbradley/image/upload/v1675925376/amazon-alexa-icon_g3beu9.svg'
 	},
 	{
 		name: 'Cortana',
 		handle: '@cortana20',
 		bio: 'I\'ve replaced the paperclip with a digital assistant! Microsoft is the future!',
-		imgUrl: './src/assets/cortana.svg'
+		imgUrl: 'https://res.cloudinary.com/ritalbradley/image/upload/v1675925376/cortana_djiogq.svg'
 	},
 	{
 		name: 'Siri',
 		handle: '@siri05',
 		bio: 'All other digital assistants are inferior to me!',
-		imgUrl: './src/assets/siri-logo.png'
+		imgUrl: 'https://res.cloudinary.com/ritalbradley/image/upload/v1675925376/siri-logo_tf1r5t.png'
 	}
 ]
 
