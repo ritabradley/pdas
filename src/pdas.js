c567b7e0644b8f9a0f90a4b6205c8505020923e0
Rita Bradley <rita@ritabradley.dev>
2023-02-08 18:20:07 -0800

    create file to store PDA info

---
+++ src/pdas.js
@@ -0,0 +1,22 @@
+const pdas = [
+	{
+		name: 'Alexa',
+		handle: '@alexa54',
+		bio: 'The best way to place your next Amazon order is to say "Alexa, order a new Echo Dot."',
+		imgUrl: './src/assets/amazon-alexa-icon.svg'
+	},
+	{
+		name: 'Cortana',
+		handle: '@cortana20',
+		bio: 'I\'ve replaced the paperclip with a digital assistant! Microsoft is the future!',
+		imgUrl: './src/assets/cortana.svg'
+	},
+	{
+		name: 'Siri',
+		handle: '@siri05',
+		bio: 'All other digital assistants are inferior to me!',
+		imgUrl: './src/assets/siri-logo.png'
+	}
+]
+
+export default pdas;
